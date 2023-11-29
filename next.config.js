2e115282ea9e412bb9446b214c17cda868d5e9b2
yuanzhisong <2010363329@qq.com>
2023-11-29 14:59:16 +0800

    解决完美解决Prop className did not match.服务器：“sc-iBPTVF lhPANS” 客户端：“sc-hBEZvw bTffbp”等类似问题

--- next.config.js
+++ next.config.js
@@ -1,4 +1,10 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {}
 
-module.exports = nextConfig
+// 完美解决Prop className did not match.服务器：“sc-iBPTVF lhPANS” 客户端：“sc-hBEZvw bTffbp”等类似问题
+// 原因：https://github.com/vercel/next.js/discussions/58919
+module.exports = {
+    compiler: {
+        styledComponents: {
+            ssr: true,
+        },
+    },
+};
